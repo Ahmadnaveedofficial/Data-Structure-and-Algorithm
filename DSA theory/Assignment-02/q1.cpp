a38670645524bb2815f73780555f006ddd192195
Ahmad Naveed <ahmadnaveedofficial05@gmail.com>
2025-10-03 22:08:21 +0500

    03-10-2025

--- DSA theory/Assignment-02/q1.cpp
+++ DSA theory/Assignment-02/q1.cpp
@@ -50,30 +50,33 @@ class LinkedList{
             head=head->next;
             delete todelete;
             cout<<"node is delete: "<<data<<endl;
+            return;
           }
           Node* temp=head;
-          while (temp->next!=NULL){
-             if (temp->next->data==data)
-             {
-               Node* todelete=temp->next;
-              temp->next=temp->next->next;
-              delete todelete;
-              cout<<"node is delete:"<<data<<endl;
-              return;
-             }
+          Node* temp2=NULL;
+          while(temp!=NULL && temp->data!=data  ){
+             temp2=temp;
              temp=temp->next;
           }
-          cout<<"node is not found : "<<data<<endl;
+          if(temp!=NULL){
+            temp2->next=temp->next;
+            delete temp;
+            cout<<"Node deleted: "<<data<<endl;
+          }else{
+            cout<<"Node is not Found: "<<data<<endl;
+          }
      }
      void searchNode(int data){
         Node* temp=head;
         while (temp!=NULL){
           if (temp->data==data) {
              cout<<"Node is found : "<<data<<endl;
-        }
+             return;
+          }
         temp=temp->next;
         }
-      cout<<"node is not found: "<<data<<endl;    
+            cout<<"Node is not Found: "<<data<<endl;
+      
      }
      void updateNode(int data,int newdata){
       Node*temp=head;
@@ -81,25 +84,64 @@ class LinkedList{
       if(temp->data==data){
           temp->data=newdata;
           cout<<"node is updated : "<<data<<" ---> "<<newdata<<endl;
+          return;
       }
       temp=temp->next;
-      }
-     }
-
-    void middleNode(){
+      } 
+      cout<<"node is not found "<<data<<endl;
+    }
+    void middleNode( bool getSecondMiddle=false){
            if(head==NULL){
             cout<<"List is empty"<<endl;
             return;
            }
             Node* slow=head;
-            Node* fast=head->next;
+            Node* fast;
+            if(getSecondMiddle){
+              fast=head;
+            }
+            else{
+              fast=head->next;
+            }
             while (fast!=NULL&&fast->next!=NULL){
               slow=slow->next;
               fast=fast->next->next;
             }
-             cout<<"middle node: "<<slow->data<<endl;
-            
+            cout<<"middle node "<<slow->data<<endl;
     }
+    
+  void makeMiddleHead(bool getSecondMiddle = false) {
+    if (head == NULL) {
+        cout << "List is empty" << endl;
+        return;
+    }
+    if (head->next == NULL) {
+        return;  
+      }
+    Node* slow = head;
+    Node* fast;
+    if (getSecondMiddle) {
+        fast = head;        
+    } else {
+        fast = head->next;  
+    }
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    Node* middleNode = slow;
+    Node*temp=head;
+    Node* temp2=head->next;
+    while (temp->next != middleNode) {
+        temp = temp->next;
+    }
+     temp->next = head;
+     head->next = middleNode->next;
+     middleNode->next = temp2;
+      head = middleNode;
+}
+
+
 };
 
 int main(){
@@ -107,11 +149,15 @@ int main(){
   cout<<"Inserting nodes..."<<endl;
   li.insertNode(1);
   li.insertNode(2);
-  li.insertNode(3);
-  li.insertNode(2);
+  li.insertNode(4);
   li.insertNode(1);
-  cout<<"Display List: "<<endl;
+  li.insertNode(2);
+  li.insertNode(3);
+  
   li.displayList();
-  cout<<"Find Middle node:"<<endl;
-  li.middleNode();
+  cout<<endl;
+  li.middleNode(true);
+
+  li.makeMiddleHead(false);
+  li.displayList();
 }
