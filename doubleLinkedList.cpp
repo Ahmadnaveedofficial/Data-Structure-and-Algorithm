b6f6ed8e269dc1a02f5ed826c7a70a97beeb5303
Ahmad Naveed <ahmadnaveedofficial05@gmail.com>
2025-10-04 22:45:39 +0500

    04-10-2024

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -46,10 +46,10 @@ class DoublyList{
             cout<<"Invalid position:"<<endl;
         }else if (pos==1)
         {
-            insertAtStart(data);
+            insertAtStart(data); 
         }else{
             Node*temp=head;
-            for(int i=1;i<pos-1&& temp!=NULL;i++){
+            for(int i=1;i<pos-1&& temp!=NULL;i++){       // insert  before the position use pos-1 and after position use only pos 
                 temp=temp->next;
             }
              if(temp!=NULL){
@@ -58,6 +58,56 @@ class DoublyList{
                     temp->next->prev=newNode;
                     temp->next=newNode;
              }
+        }
+    }
+
+    void deleteAtStart(){
+        if(head!=NULL){
+            Node* temp=head;
+            head=head->next;
+            head->prev=NULL;
+            temp->next=NULL;
+            delete temp;
+
+        }
+    }
+    void deleteAtEnd(){
+        if(head!=NULL){
+            Node* temp=tail;
+            tail=tail->prev;
+            tail->next=NULL;
+            temp->prev=NULL;
+            delete temp;
+        }
+    }
+
+    void deleteAtPosition(int pos){
+        if(pos<1){
+            cout<<"Invalid Position"<<endl;
+            return;
+        }
+        if(pos==1){
+            deleteAtStart();
+            return;
+        }
+        else{
+            Node* temp=head;
+            for(int i=1;i<pos-1;i++){   // delete before the position use pos-1 and after position use only pos
+                temp=temp->next;
+                if(temp==NULL){
+                    break;
+                }
+            }
+            if(temp!=NULL && temp->next!=NULL){
+                Node* temp2=temp->next;
+                temp->next=temp2->next;
+                if(temp2->next!=NULL){
+                    temp2->next->prev=temp;
+                }
+                temp2->next=NULL;
+                temp2->prev=NULL;
+                delete temp2;
+            }
         }
     }
 
@@ -78,5 +128,8 @@ int main(){
     li.insertAtStart(3);
     li.insertAtEnd(4);
     li.insertAtPostion(3,4);
+    // li.deleteAtStart();
+    // li.deleteAtEnd();
+    li.deleteAtPosition(3);
     li.display();
 }
