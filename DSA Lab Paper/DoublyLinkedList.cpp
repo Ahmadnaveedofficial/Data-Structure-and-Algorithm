568504293b90e611bc5950866aafde274ceb8a0c
Ahmad Naveed <ahmadnaveedofficial05@gmail.com>
2025-12-07 18:12:43 +0500

    PAPER

---
+++ DSA Lab Paper/DoublyLinkedList.cpp
@@ -0,0 +1,217 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    string name;
+    int rollNo;
+    int marks;
+    Node *next;
+    Node *prev;
+    Node(string name, int rollNo, int marks)
+    {
+        this->name = name;
+        this->marks = marks;
+        this->rollNo = rollNo;
+        next = prev = NULL;
+    }
+};
+
+class DoublyList
+{
+private:
+    Node *head;
+    Node *tail;
+
+public:
+    DoublyList()
+    {
+        head = tail = NULL;
+    }
+
+    void insertAtStart(string name, int rollNo, int marks)
+    {
+        Node *newNode = new Node(name, rollNo, marks);
+        if (head == NULL)
+        {
+            head = tail = newNode;
+            return;
+        }
+        newNode->next = head;
+        head->prev = newNode;
+        newNode->prev = NULL;
+        head = newNode;
+    }
+
+    void insertAtLast(string name, int rollNo, int marks)
+    {
+        Node *newNode = new Node(name, rollNo, marks);
+        if (head == NULL)
+        {
+            head = tail = newNode;
+            return;
+        }
+        newNode->prev = tail;
+        tail->next = newNode;
+        tail = newNode;
+    }
+
+    void display()
+    {
+        Node *temp = head;
+        if (temp == NULL)
+        {
+            cout << "List is empty" << endl;
+            return;
+        }
+        while (temp != NULL)
+        {
+            cout << "[Name: " << temp->name
+                 << ", Roll: " << temp->rollNo
+                 << ", Marks: " << temp->marks << "] <==> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+
+    void deleteAtStart()
+    {
+        if (head == NULL)
+        {
+            cout << "Doubly Linked List is Empty" << endl;
+            return;
+        }
+        if (head->next == NULL)
+        {
+            delete head;
+            head = tail = NULL;
+        }
+        else
+        {
+            Node *temp = head;
+            head = head->next;
+            head->prev = NULL;
+            delete temp;
+        }
+    }
+
+    void deleteAtEnd()
+    {
+        if (head == NULL)
+        {
+            cout << "Doubly Linked List is Empty" << endl;
+            return;
+        }
+        if (head == tail)
+        {
+            delete head;
+            head = tail = NULL;
+        }
+        else
+        {
+            Node *temp = tail;
+            tail = tail->prev;
+            tail->next = NULL;
+            temp->prev = NULL;
+            delete temp;
+        }
+    }
+
+    void insertAtPosition(int pos, string name, int rollNo, int marks)
+    {
+        if (pos < 1)
+        {
+            cout << "Invalid position" << endl;
+            return;
+        }
+        if (pos == 1)
+        {
+            insertAtStart(name, rollNo, marks);
+            return;
+        }
+        Node *temp = head;
+        for (int i = 1; i < pos - 1 && temp != NULL; i++)
+        {
+            temp = temp->next;
+        }
+        if (temp == NULL)
+        {
+            cout << "Invalid position" << endl;
+            return;
+        }
+        Node *newNode = new Node(name, rollNo, marks);
+        newNode->next = temp->next;
+        newNode->prev = temp;
+        if (temp->next != NULL)
+        {
+            temp->next->prev = newNode;
+        }
+        else
+        {
+            tail = newNode;
+        }
+        temp->next = newNode;
+    }
+
+    void deleteAtPosition(int pos)
+    {
+        if (pos < 1 || head == NULL)
+        {
+            cout << "Invalid position" << endl;
+            return;
+        }
+        if (pos == 1)
+        {
+            deleteAtStart();
+            return;
+        }
+        Node *temp = head;
+        for (int i = 1; i < pos - 1 && temp != NULL; i++)
+        {
+            temp = temp->next;
+        }
+        if (temp == NULL || temp->next == NULL)
+        {
+            cout << "Invalid position" << endl;
+            return;
+        }
+        Node *temp2 = temp->next;
+        temp->next = temp2->next;
+        if (temp2->next != NULL)
+        {
+            temp2->next->prev = temp;
+        }
+        else
+        {
+            tail = temp;
+        }
+        delete temp2;
+    }
+};
+
+int main()
+{
+    DoublyList l;
+    l.insertAtStart("Ahmad Naveed", 149, 89);
+    l.insertAtStart("Mohsin Naveed", 117, 87);
+    l.insertAtStart("Ahsan Naveed", 97, 78);
+    l.insertAtLast("Nawab", 353, 73);
+
+    l.display();
+    cout << endl;
+
+    l.deleteAtStart();
+    l.display();
+
+    l.deleteAtEnd();
+    cout << endl;
+    l.display();
+
+    l.insertAtPosition(2, "Inserted Mid", 200, 90);
+    l.display();
+
+    l.deleteAtPosition(2);
+    l.display();
+
+    return 0;
+}
